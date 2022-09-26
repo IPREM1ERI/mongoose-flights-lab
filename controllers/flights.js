fce5867d92a8a4c3bf08edb778156d4dcc26072f
davidv <davidvazquezgit@gmail.com>
2022-09-26 14:28:29 -0700

    add index route and ctrl

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,1 +1,17 @@
 import { Flight } from "../models/flight.js";
+
+function index(req, res) {
+  Flight.find({})
+  .then(flights => {
+    res.render('flights/index',{
+      airline,
+      airport,
+      flightNo,
+      departs
+    })
+  })
+}
+
+export {
+  index
+}
--- models/flight.js
+++ models/flight.js
@@ -3,11 +3,29 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema
 
 const flightSchema = new Schema ({
-  airline: String,
-  airport: String,
-  flightNo: Number,
-  departs: Date
+  airline: {
+    type:String,
+    enum: ['Southwest','United','American']
+  },
+  airport: {
+    type: String,
+    enum:['AUS','DFW','LAX','DEN','SAN'],
+    default:'DEN'
+  },
+  flightNo: {
+    type: Number, min: 10, max: 9999
+  },
+  departs: {
+    type: Date,
+    default: oneYearFromNow()
+  }
 })
+
+function oneYearFromNow() {
+  const today = new Date()
+  today.setFullYear(today.getFullYear() + 1)
+  return today
+}
 
 const Flight = mongoose.model('Flight', flightSchema)
 
